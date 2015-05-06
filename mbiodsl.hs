c405af538b725cf7a798da95ec587eeb4975d1a6
Wazim Mohammed Ismail <wazimoha@indiana.edu>
2015-05-06 16:00:49 -0400

    Changes to mbiodsl.hs

--- mbiodsl.hs
+++ mbiodsl.hs
@@ -13,17 +13,17 @@ import Text.PrettyPrint.HughesPJClass
 data Nucleotide = A | C | G | T | N deriving (Eq,Show)
 type DNA = [Nucleotide]
 
-data Feature = Single (Int,Int) | Multipart (Int,Int) [Feature]  deriving (Eq,Show)
+data Feature = Node (Int,Int) [Feature]  deriving (Eq,Show)
 
 instance Pretty Feature where
-	pPrint (Single (a,b)) = text "Single (" <> pPrint a <> text "," <> pPrint b <> text ")"
-	pPrint (Multipart (a,b) parts) = vcat $ [ text "Multipart (" <> pPrint a <> text "," <> pPrint b <> text ")" ] ++ map (nest 3 . pPrint) parts
+	pPrint (Node (a,b) parts) = vcat $ [ text "Node (" <> pPrint a <> text "," <> pPrint b <> text ")" ] ++ map (nest 3 . pPrint) parts
 
 type FindFeature = (ErrorT String Identity) Feature
 
 data Signal
 	= Signal (DNA -> Maybe Feature)
 	| Signal :> Signal
+	| Part Int Signal
 	| (Signal,Int) :#: Signal
 
 motif :: String -> Signal
@@ -31,42 +31,24 @@ motif m =
   let kmpTable = build $ dna m
   in Signal (\d -> 
 				let matches = match kmpTable d
-				in if (length matches) > 0 then Just (Single ((head matches)+1,(head matches)+(length m))) else Nothing)
+				in if (length matches) > 0 then Just (Node ((head matches)+1,(head matches)+(length m)) []) else Nothing)
 
-{--
 invertedRepeats :: Signal
 invertedRepeats = Signal (\d ->
   	let
       aligns = (align cost d (reverseComplement d))
       l = length d
 		in if (length aligns) == 0 
-				then Nil
+				then Nothing
 				else let (p,q,r,s) = (head aligns)
-							in Multipart (p,l-r+1) [Single (1,q-p+1), Single (q-p+1,l-s-p+2), Single (l-s-p+2,l-r-p+2)])
+							in Just $ Node (p,l-r+1) [Node (1,q-p+1) [], Node (q-p+1,l-s-p+2) [], Node (l-s-p+2,l-r-p+2) []])
 
 gcRich :: Signal
-gcRich = Signal (\d -> if (toRational (length [x | x <- d, x == G || x == C])) > (0.75 * (toRational (length d))) then Single (1, (length d)) else Nil)
-
-part :: Int -> Feature -> Feature
-part _ Nil = Nil
-part _ (Single _) = Nil
-part i (Multipart (a,b) parts) = if (i >= 0 && i < (length parts)) then (scale a (parts!!i)) else Nil
+gcRich = Signal (\d -> if (toRational (length [x | x <- d, x == G || x == C])) > (0.75 * (toRational (length d))) then Just $ Node (1, (length d)) [] else Nothing)
 
 slice from to xs = take (to - from + 1) (drop (from-1) xs)
 
 replaceAtIndex n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
-
-scale :: Int -> Feature -> Feature
-scale i Nil = Nil
-scale i (Single (a,b)) = Single (a+i-1,b+i-1)
-scale i (Multipart (a,b) parts) = Multipart (a+i-1,b+i-1) parts
-
-interval :: Feature -> Maybe (Int,Int)
-interval Nil = Nothing
-interval (Single inter) = Just inter
-interval (Multipart inter parts) = Just inter
-
---}
 
 runFindFeature :: FindFeature -> (Either String Feature)
 runFindFeature ff = runIdentity (runErrorT ff)
@@ -76,62 +58,41 @@ findFeature (Signal s) d = case s d of
 														Nothing -> throwError ("No feature found!")
 														Just val -> return val
 
-{--
-findFeature (a :> b) d
-	= case (findFeature a d) of 
-			Nil -> Nil
-			Single (start1,end1) -> case (findFeature b (drop end1 d)) of
-															Nil -> Nil
-															Single (start2,end2) -> Multipart (start1,end1+end2) 
-																												[Single (1,end1-start1+1), 
-																												Single (end1-start1+1,end1+start2-start1+1), 
-																												Single (end1+start2-start1+1,end1+end2-start1+1)]
-															Multipart (start2,end2) parts -> Multipart (start1,end1+end2) 
-																																[Single (1,end1-start1+1), 
-																																Single (end1-start1+1,end1+start2-start1+1), 
-																																Multipart (end1+start2-start1+1,end1+end2-start1+1) parts]
-			Multipart (start1,end1) parts1 -> case (findFeature b (drop end1 d)) of
-                              Nil -> Nil
-                              Single (start2,end2) -> Multipart (start1,end1+end2) 
-																												[Multipart (1,end1-start1+1) parts1, 
-																												Single (end1-start1+1,end1+start2-start1+1), 
-																												Single (end1+start2-start1+1,end1+end2-start1+1)]
-                              Multipart (start2,end2) parts2 -> Multipart (start1,end1+end2) 
-																																	[Multipart (1,end1-start1+1) parts1, 
-																																	Single (end1-start1+1,end1+start2-start1+1), 
-																																	Multipart (end1+start2-start1+1,end1+end2-start1+1) parts2]
+findFeature (a :> b) d = do
+	(Node (start1,end1) parts1) <- (findFeature a d)
+	(Node (start2,end2) parts2) <- (findFeature b (drop end1 d))
+	return $ Node (start1,end1+end2) [Node (1,end1-start1+1) parts1, 
+																		Node (end1-start1+1,end1+start2-start1+1) [], 
+																		Node (end1+start2-start1+1,end1+end2-start1+1) parts2]
 
-findFeature ((a,i) :#: b) d
-	= case (findFeature a d) of
-			Nil -> Nil
-			Single (start1,end1) -> if i /= 0 
-															then Nil 
-															else case (findFeature b (slice start1 end1 d)) of
-																Nil -> Nil
-																Single (start2,end2) -> Multipart (start1,end1) 
-																													[Single (1,start2), 
-																													Single (start2,end2), 
-																													Single (end2,end1-start1+1)]
-																Multipart (start2,end2) parts -> Multipart (start1,end1)
-																																	[Single (1,start2),
-																																	Multipart (start2,end2) parts,
-																																	Single (end2,end1-start1+1)]
-			Multipart (start1,end1) parts1 -> 
-				case interval (part i (Multipart (start1,end1) parts1)) of
-					Nothing -> Nil
-					Just (start2,end2) -> case (findFeature b (slice start2 end2 d)) of
-															Nil -> Nil
-															Single (start3,end3) -> Multipart (start1,end1) 
-																												(replaceAtIndex i (Multipart (start2-start1+1,end2-start1+1)
-																																						[Single (1,start3),
-																																						Single (start3,end3),
-																																						Single (end3,end2-start2+1)]) parts1)
-															Multipart (start3,end3) parts -> Multipart (start1,end1)
-																																(replaceAtIndex i (Multipart (start2-start1+1,end2-start1+1)
-																																										[Single (1,start3),
-																																										Multipart (start3,end3) parts,
-																																										Single (end3,end2-start2+1)]) parts1)
---}																
+findFeature (Part i s) d = do
+	(Node (start,end) parts) <- (findFeature s d)
+	if (i < 0 || i >= (length parts)) 
+	then throwError ("No feature found!")
+	else 
+		let (Node (starti,endi) partsi) = parts!!i in
+		return $ (Node (starti+start-1,endi+start-1) partsi)
+
+findFeature ((a,i) :#: b) d = do
+	(Node (start1,end1) parts1) <- (findFeature a d)
+	if (parts1 == []) 
+	then do
+		(Node (start2,end2) parts2) <- (findFeature b (slice start1 end1 d))
+		return $ Node (start1,end1) [Node (1,start2) [], Node (start2,end2) parts2, Node (end2,end1-start1+1) []]
+	else if (i < 0 || i >= (length parts1))
+	then do
+		throwError ("No feature found!")
+	else
+		let 
+			(Node (starti,endi) partsi) = parts1!!i
+			start2 = starti+start1-1
+			end2 = endi+start1-1
+		in do
+			(Node (start3,end3) parts3) <- (findFeature b (slice start2 end2 d))
+			return $ Node (start1,end1) (replaceAtIndex i (Node (start2-start1+1,end2-start1+1) 
+																											[Node (1,start3) [], 
+																											Node (start3,end3) parts3, 
+																											Node (end3,end2-start2+1) []]) parts1)														
 
 
 -- Helper functions to convert Char/String to Nucleotide/DNA
@@ -166,9 +127,11 @@ main = do
 	let fwd = (dna "GGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCG")
 	let rev = (dna "CGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCC")
 	putStrLn $ show $ (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
-	putStrLn $ show $ runFindFeature $ findFeature (motif "ATG") (dna "TTCTTAGATGCGGC")
--- 	putStrLn $ prettyShow $ findFeature invertedRepeats (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
--- 	putStrLn $ prettyShow $ findFeature ((invertedRepeats,0) :#: gcRich) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
--- 	putStrLn $ prettyShow $ findFeature ((((invertedRepeats,0) :#: gcRich),1) :#: transposase) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
--- 	putStrLn $ prettyShow $ findFeature ((((invertedRepeats,0) :#: gcRich),1) :#: (transposase :> pif)) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
+--	putStrLn $ prettyShow $ runFindFeature $ findFeature (motif "ATG") (dna "TTCTTAGATGCGGC")
+--	putStrLn $ prettyShow $ runFindFeature $ findFeature ((motif "ATG") :> (motif "GGC")) (dna "TTCTTAGATGCGGC")
+	putStrLn $ prettyShow $ runFindFeature $ findFeature invertedRepeats (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
+--	putStrLn $ prettyShow $ runFindFeature $ findFeature (Part 0 invertedRepeats) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
+	putStrLn $ prettyShow $ runFindFeature $ findFeature ((invertedRepeats,0) :#: gcRich) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
+	putStrLn $ prettyShow $ runFindFeature $ findFeature ((((invertedRepeats,0) :#: gcRich),1) :#: transposase) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
+	putStrLn $ prettyShow $ runFindFeature $ findFeature ((((invertedRepeats,0) :#: gcRich),1) :#: (transposase :> pif)) (dna "GGAGGCATGCGGCGGCGGCGATAATCGGCGAGGACGGCGGCGGCGTTATCTAGGTATCGACTCGTTTACGGCGCGAGCTGGGCTTTGATTCGCCGCCGCCGTCCTCGCCGATTATCGCCGCCGCCTTTCTGCT")
 
